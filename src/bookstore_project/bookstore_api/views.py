98c775df03fef87c6604cff9d8a7098db5c62631
harmannd <derekhar2468@gmail.com>
2018-02-16 18:31:04 -0600

    Linked static css files

--- src/bookstore_project/bookstore_api/views.py
+++ src/bookstore_project/bookstore_api/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the bookstore_api index!!")
+    return render(request, 'base.html')
 
