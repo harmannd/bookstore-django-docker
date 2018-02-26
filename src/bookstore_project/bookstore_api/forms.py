8ef912b7f0a70b00a0af849698a8acd1f3b974ba
harmannd <derekhar2468@gmail.com>
2018-02-26 15:16:24 -0600

    Changed to a filter form and revamped index post requests

--- src/bookstore_project/bookstore_api/forms.py
+++ src/bookstore_project/bookstore_api/forms.py
@@ -1,4 +1,10 @@
 from django import forms
 
+from .models import Book
+
 class SearchForm(forms.Form):
-    search = forms.CharField(label='Search Text', max_length=100)
+    search = forms.CharField(label='Search Text', max_length=100, required=False)
+
+class FilterForm(forms.Form):
+    file_type = forms.MultipleChoiceField(choices=Book.FILE_TYPES, widget=forms.CheckboxSelectMultiple(), required=False)
+    language = forms.MultipleChoiceField(choices=Book.LANGUAGES, widget=forms.CheckboxSelectMultiple(), required=False)
--- src/bookstore_project/bookstore_api/views.py
+++ src/bookstore_project/bookstore_api/views.py
@@ -2,26 +2,34 @@ from django.shortcuts import render, get_object_or_404, \
     get_list_or_404
 
 from .models import Book
-from .forms import SearchForm
+from .forms import SearchForm, FilterForm
 
 def index(request):
     if request.method == 'POST':
-        form = SearchForm(request.POST)
-        if form.is_valid():
-            #process data
-            search = form.cleaned_data['search']
-            books = get_list_or_404(Book.objects.filter(Book.title==search))
+        search_form = SearchForm(request.POST)
+        filter_form = FilterForm(request.POST)
+        if 'search' in request.POST:
+            #books = get_list_or_404(Book.objects.filter(Book.title==search))
+            books = get_list_or_404(Book)[:6]
+        #if a filter is selected
+        elif 'filter' in request.POST and filter_form.is_valid():
+            #determine which file_types
+            file_types = filter_form.cleaned_data.get('file_type')
+            books = get_list_or_404(Book.objects.filter(Book.file_type in file_types))[:12]
+        else:
+            #nothing filtered or searched
+            books = get_list_or_404(Book)[:10]
     else:
-        form = SearchForm()
+        search_form = SearchForm()
+        filter_form = FilterForm()
         books = get_list_or_404(Book)[:10]
     return render(
         request,
         'bookstore_api/index.html',
         {
-            'FILE_TYPES': Book.FILE_TYPES,
-            'LANGUAGES': Book.LANGUAGES,
             'books': books,
-            'form': form
+            'search_form': search_form,
+            'filter_form': filter_form
         }
     )
 
